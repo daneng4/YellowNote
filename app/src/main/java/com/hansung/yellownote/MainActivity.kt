8db3493e78963ab7ce6389b11cf0ab7dcf3d84ed
HyunaJo <69022662+HyunaJo@users.noreply.github.com>
2022-07-26 06:51:07 +0900

    add, select btn 생성

--- app/src/main/java/com/hansung/yellownote/MainActivity.kt
+++ app/src/main/java/com/hansung/yellownote/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.hansung.yellownote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.hansung.yellownote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +13,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // toolbar 설정
+        var toolbar = binding.introToolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false) // toolbar 제목 표시 유무
     }
 
+    // toolbar에 menu item 넣기
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.intro_toolbar, menu)
+        return true
+    }
 
+    //item 버튼 클릭 했을 때
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+//            android.R.id.home -> {
+//                return true
+//            }
+            R.id.selectNote -> {
+                //노트 선택 버튼 누른 경우
+                println("Click selectNote menu btn")
+                return true
+            }
+            R.id.addNote -> {
+                //노트 추가 버튼 누른 경우
+                println("Click addNote menu btn")
+                val intent = Intent(this, NoteActivity::class.java)
+                intent.putExtra("filePath",this.cacheDir.absolutePath+"/wadifoo.pdf")
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
 }
