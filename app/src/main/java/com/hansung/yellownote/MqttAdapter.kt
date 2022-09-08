bb1c3048b2c7a112d057da5f6802ba8503fcb51b
hanseongbugi <bhsung1@naver.com>
2022-09-08 20:48:31 +0900

    Coroutine_init

--- app/src/main/java/com/hansung/yellownote/MqttAdapter.kt
+++ app/src/main/java/com/hansung/yellownote/MqttAdapter.kt
@@ -1,58 +1,123 @@
 package com.hansung.yellownote
 
 import android.util.Log
+import kotlinx.coroutines.*
 import org.eclipse.paho.client.mqttv3.*
 import java.io.Serializable
 
-class MqttAdapter{
+class MqttAdapter() {
     private lateinit var client: MqttClient
-    constructor(){
-        try{
-            client= MqttClient("tcp://223.194.129.17:1883",MqttClient.generateClientId(),null)
-            if(!client.isConnected){
-                client.connect()
-            }
-            client.subscribe("result")
-            client.setCallback(object:MqttCallback{
-                override fun connectionLost(cause: Throwable?) {
-                    Log.d("MqttService","Connection Lost")
-                }
-
-                override fun messageArrived(topic: String?, message: MqttMessage?) {
-                    if(topic=="result"){
-
+    private lateinit var scope: CoroutineScope
+    private lateinit var world:String
+    init {
+        connectClient()
+    }
+    private fun connectClient(){
+        scope= CoroutineScope(Dispatchers.IO).apply {
+            launch {
+                try {
+                    client =
+                        MqttClient("tcp://223.194.133.119:1883", MqttClient.generateClientId(), null)
+                    if (!client.isConnected) {
+                        println("연결")
+                        withContext(Dispatchers.Main) {
+                            client.connect()
+                        }
                     }
-                }
+                    client.subscribe("result")
+                    client.setCallback(object : MqttCallback {
+                        override fun connectionLost(cause: Throwable?) {
+                            Log.d("MqttService", "Connection Lost")
+                        }
 
-                override fun deliveryComplete(token: IMqttDeliveryToken?) {
-                    Log.d("MqttService","Delivery Complete")
+                        override fun messageArrived(topic: String?, message: MqttMessage?) {
+                            if (topic == "result") {
+                                val msg=message
+                                world=msg.toString()
+                                println(world)
+                            }
+                        }
+
+                        override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                            Log.d("MqttService", "Delivery Complete")
+                        }
+                    })
+                } catch (e: MqttException) {
+                    e.printStackTrace()
                 }
-            })
-        }catch (e:MqttException){
-            e.printStackTrace()
+            }
         }
     }
-    fun sendPixelMessage(drawings:ByteArray){
-        try{
-            System.out.println("sendPixels")
-            if(!client.isConnected)
-                client.connect()
-            client.publish("easyocr/hangul",MqttMessage(drawings))
-        }catch (e:MqttException){
-            e.printStackTrace()
+    fun sendHangulPixelMessage(drawings:ByteArray){
+        scope= CoroutineScope(Dispatchers.IO).apply {
+            launch {
+                try {
+                    if (!client.isConnected) {
+                        withContext(Dispatchers.Main) {
+                        client.connect()
+                        }
+                    }
+
+                      client.publish("easyocr/hangul", MqttMessage(drawings))
+
+                } catch (e: MqttException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+    fun sendEnglishPixelMessage(drawings:ByteArray){
+        scope= CoroutineScope(Dispatchers.IO).apply {
+            launch {
+                try {
+                    if (!client.isConnected) {
+                        withContext(Dispatchers.Main) {
+                            client.connect()
+                        }
+                    }
+
+                    client.publish("easyocr/english", MqttMessage(drawings))
+
+                } catch (e: MqttException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+    fun sendNumberPixelMessage(drawings:ByteArray){
+        scope= CoroutineScope(Dispatchers.IO).apply {
+            launch {
+                try {
+                    if (!client.isConnected) {
+                        withContext(Dispatchers.Main) {
+                            client.connect()
+                        }
+                    }
+
+                    client.publish("easyocr/english", MqttMessage(drawings))
+
+                } catch (e: MqttException) {
+                    e.printStackTrace()
+                }
+            }
         }
     }
     fun sendImageSizeMessage(width:Int){
-        val message=width.toString()
-        println(width)
-        println(message)
-        println(message.toByteArray())
-        try{
-            if(!client.isConnected)
-                client.connect()
-            client.publish("imagesize",MqttMessage(message.toByteArray()))
-        }catch(e:MqttException){
-            e.printStackTrace()
+        scope= CoroutineScope(Dispatchers.IO).apply {
+            launch {
+                val message = width.toString()
+                try {
+                    if (!client.isConnected) {
+                        withContext(Dispatchers.Main) {
+                            client.connect()
+                        }
+                    }
+                    client.publish("imagesize", MqttMessage(message.toByteArray()))
+                }catch (e: MqttException) {
+                    e.printStackTrace()
+                }
+            }
         }
     }
+
 }
